859562dc349237c2b441ee1f3cbc9c6127f8dc10
Dan Weiner <daweiner16@gmail.com>
2016-04-26 10:27:56 -0400

    adding initial files

---
+++ practice.py
@@ -0,0 +1,29 @@
+print('hi')
+
+# Branching programs - using conditionals (if statements)
+# x = int( raw_input("Enter an integer: "))
+# if x%2 == 0:
+#   print('')
+#   print('Even')
+# else:
+#   print('')
+#   print('Odd')
+# print('Done with conditional')
+
+
+# Nested conditionals
+# x = int( raw_input("Enter an integer: "))
+# if x%2 == 0:
+#   if x%3 == 0:
+#     print('Divisible by 2 and 3')
+#   else:
+#     print('Divisible by 2 and not 3')
+# elif x%3 == 0:
+#   print('Divisible by 3 and not 2')
+# else:
+#   print('Not divisible by 2 or 3')
+
+
+# Using compound Booleans
+
+#x > y and x < z for example
