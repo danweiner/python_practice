11ddf8063adddce7f29c25d19b5ff3ca8886caee
Dan Weiner <daweiner16@gmail.com>
2016-04-27 11:55:18 -0400

    adding find root function

--- practice.py
+++ practice.py
@@ -149,21 +149,141 @@ print('hi')
 
 # A simple function to return the max of x or y
 
-def max(x, y):
-  if x > y:
-    print(x)
-  else:
-    print(y)
+# def max(x, y):
+#   if x > y:
+#     print(x)
+#   else:
+#     print(y)
 
-# We then have to call the function
-# We can bind the result to a variable
+# # We then have to call the function
+# # We can bind the result to a variable
 
-z = max(3, 4)
-
-
+# z = max(3, 4)
 
 
+# Successive multiplication
 
+# x = float(raw_input('Enter a number: '))
+# p = int(raw_input('Enter an integer power: '))
+
+# result = 1
+
+# for turn in range(p):
+#   print('iteration: ' + str(turn) + ' current result: ' + str(result))
+#   result = result * x
+
+# # Now capture it in a procedure
+
+# def iterativePower(x,p):
+#   result = 1
+#   for turn in range(p):
+#     print('iteration: ' + str(turn) + ' current result: ' + str(result))
+#     result = result * x
+#   print(result)
+
+# iterativePower(5, 6)
+
+
+# Variable Binding
+
+# def f(x):
+#   y = 1
+#   x = x + y
+#   print('x = ' + str(x))
+#   return x
+
+# x = 3
+# y = 2
+# z = f(x)
+# # Print values are x = 4, z = 4, x = 3, y = 2
+# # control reverts to global environment where values of x, y, and z are visible
+# print('z = ' + str(z))
+# print('x = ' + str(x))
+# print('y = ' + str(y))
+# each function call creates a new environment
+
+
+# Another example
+
+# def square(x):
+#   return x*x
+
+# def twoPower(x,n):
+#   while n>1:
+#     x = square(x)
+#     n = n/2
+#   return x
+
+# x = 5
+# n = 1
+# print(square(5))
+# print(twoPower(2, 8))
+
+
+# Root Finding Using Bisection Search
+
+# def findRoot1(x,power,epsilon):
+#   low = 0
+#   high = x
+#   ans = (high + low) / 2.0
+#   while abs(ans**power - x) > epsilon:
+#     if ans**power < x:
+#       low = ans
+#     else:
+#       high = ans
+#     ans = (high + low) / 2.0
+#   return ans 
+
+# print(findRoot1(25, 2, .001)) 
+# print(findRoot1(27, 3, .001))
+# # Results in infinite loop
+# print(findRoot1(-27, 3, .001))
+
+
+# Fixing function for negative number
+# def findRoot2(x,power,epsilon):
+#   if x < 0 and power%2 == 0:
+#     return None
+#   # can't find even powered root of negative number
+#   low = min(0,x)
+#   high = max(0,x)
+#   ans = (high + low) / 2.0
+#   while abs(ans**power - x) > epsilon:
+#     if ans**power < x:
+#       low = ans
+#     else:
+#       high = ans
+#     ans = (high + low) / 2.0
+#   return ans 
+
+# print(findRoot2(25, 2, .001)) 
+# print(findRoot2(27, 3, .001))
+# print(findRoot2(-27, 3, .001))
+# # But this fails
+# print(findRoot2(0.25, 2, .001))
+
+
+# Fixing function for decimal number
+def findRoot3(x,power,epsilon):
+  if x < 0 and power%2 == 0:
+    return None
+  # can't find even powered root of negative number
+  low = min(-1,x)
+  high = max(1,x)
+  ans = (high + low) / 2.0
+  while abs(ans**power - x) > epsilon:
+    if ans**power < x:
+      low = ans
+    else:
+      high = ans
+    ans = (high + low) / 2.0
+  return ans 
+
+print(findRoot3(25, 2, .001)) 
+print(findRoot3(27, 3, .001))
+print(findRoot3(-27, 3, .001))
+# But this fails
+print(findRoot3(0.25, 2, .001))
 
 
 
